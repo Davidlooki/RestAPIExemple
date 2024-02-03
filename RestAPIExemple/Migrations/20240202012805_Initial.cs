bd0547131eb319c301e5e1c2960a389c6a61a26d
Davidlooki <davidlooki@hotmail.com>
2024-02-03 20:16:29 -0300

    Migrations and app settings

---
+++ RestAPIExemple/Migrations/20240202012805_Initial.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RestAPIExemple.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pokemons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pokemons",
+                columns: new[] { "Id", "Description", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "...", "Bulbasaur", 0 },
+                    { 2, "...", "Ivysaur", 0 },
+                    { 3, "...", "Venosaur", 0 },
+                    { 4, "...", "Charmander", 0 },
+                    { 5, "...", "Charmeleon", 0 },
+                    { 6, "...", "Charizard", 0 },
+                    { 7, "...", "Squirtle", 0 },
+                    { 8, "...", "Wartortle", 0 },
+                    { 9, "...", "Blastoise", 0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pokemons");
+        }
+    }
+}
